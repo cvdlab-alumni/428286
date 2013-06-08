f9b80328913b248bc546f7d23c72e28e5ced5141
eleonoracappelli <elemarzo90@hotmail.it>
2013-06-08 18:58:01 +0200

    homework3

--- 2013-06-07/javascript/exercise2.js
+++ 2013-06-07/javascript/exercise2.js
@@ -1,26 +1,3 @@
-function beziera_2D(f){ return BEZIER(S1)(f)}
-function beziera_1D(controlp){ return BEZIER(S0)(controlp)}
-var dom1D = INTERVALS(1)(32)
-var dom2D = DOMAIN([[0,1],[0,1]])([30,30])
-
-function b_Dom1D(controlpoints){
-	return MAP(BEZIER(S0)(controlpoints))(dom1D)
-}
-
-function b_Dom2D(functions){
-	return MAP(BEZIER(S1)(functions))(dom2D) 
-}
- 
-
-
-function cerc(r,z){
-var points = [[1,0,z],[1,1,z],[0,1.62,z],[-1.22,1.22,z],[-2,0,z],[-1.22,-1.22,z],[0,-1.63,z],[1,-1,z],[1,0,z]];
-var c = points.map(function(point){return [point[0]*r,point[1]*r,point[2]]})
-var cerchio = beziera_1D(c);
-return cerchio;
-}
-
-
 
 var domain = DOMAIN([[0, 60], [0, 60]])([24, 24]);
 
@@ -48,7 +25,7 @@ DRAW(COLOR([85/255,104/255,50/255])(m));
 
 
 //exercise2
-var dominioLago = DOMAIN([[20, 30], [20, 50]])([1,1]);
+var domLago = DOMAIN([[20, 30], [20, 50]])([1,1]);
 
 var lago = function (d,z) {
     
@@ -60,6 +37,6 @@ var lago = function (d,z) {
 }
 
 
-var l = MAP(lago)(dominioLago);
+var l = MAP(lago)(domLago);
 
 DRAW(COLOR([60/255,179/255,113/255])(l))
